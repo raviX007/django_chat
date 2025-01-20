79cad3a987ae703d82aab19431d71be21fde9f35
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-01-20 18:04:56 +0530

    removed postgres

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -68,15 +68,8 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
