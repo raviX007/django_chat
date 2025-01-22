ac74a99199147b040b90150365e2b4309396e732
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-01-23 01:16:15 +0530

    Setting Changes

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -69,13 +69,15 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default='17756'),
     }
 }
+
+APP_PORT = config('APP_PORT', default='8000')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
